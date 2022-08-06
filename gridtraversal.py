2be712dae63c28d84966e6643f430ddd17cad3d9
soapp88 <91401787+soapp88@users.noreply.github.com>
2022-08-06 16:54:19 -0700

    Add files via upload

--- gridtraversal.py
+++ gridtraversal.py
@@ -34,7 +34,7 @@ class Solution:
         grid = [[1 for _ in range(n)] for _ in range(m)]
 
         for i in range(1, m):
-            for j in range(1, m):
+            for j in range(1, n):
                 grid[i][j] = grid[i-1][j] + grid[i][j-1]
         
         return grid[-1][-1]
